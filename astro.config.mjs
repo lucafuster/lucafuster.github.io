bd2b3dc18eb10bc89a0eff88ab308e5e6a38b266
Luca Fuster <luca.fuster@telecom-sudparis.eu>
2025-12-24 00:09:22 +0100

    Config base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  // Remplace par ton URL GitHub finale
+  // Remplace par ton URL exacte
   site: 'https://lucafuster.github.io',
-  base: '/lucafuster',
+  // Ajoute le nom de ton dépôt ici
+  base: '/lucafuster', 
   integrations: [tailwind()],
 });
